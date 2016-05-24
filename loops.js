9ece09c2bb384fbb4515df4ef5db5e3c7d1a7536
Grace <grace.h.gamble@gmail.com>
2016-05-24 20:55:41 +0000

    Final Update to Loop Practice #1

--- loops.js
+++ loops.js
@@ -13,10 +13,42 @@ for (var i = 1; i < 65; i*=2) {
 */
 
 //Part 3
+/*
 for (var i = 1; i < 4; i++) {
     for (var j = 1; j < 4; j++) {
         console.log(i);
     }
 }
+*/
 
+//Part 4
+/*
+for (var i = 0; i < 11; i += 2) {
+    console.log(i);
+}
+*/
+
+//Part 5
+/*
+for (var i = 3; i < 16; i +=3) {
+    console.log(i);
+}
+*/
+
+//Part 6
+/*
+for (var i = 9; i > 0; i --) {
+    console.log(i);
+}
+*/
+
+//Part 7
+/*
+for (var j = 1; j < 4; j ++) {
+    for (var i = 0; i < 4; i ++) {
+        console.log(i);
+        
+    }
+}
+*/
 
