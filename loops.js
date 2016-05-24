17c1b40be1910df3808ae08318ee7e690f9610cd
Grace <grace.h.gamble@gmail.com>
2016-05-24 18:28:14 +0000

    First Update to Loop Practice #1

---
+++ loops.js
@@ -0,0 +1,22 @@
+//Part 1
+/*
+for (var i = 0; i < 501; i+=100) {
+    console.log(i);
+}
+*/
+
+//Part 2
+/*
+for (var i = 1; i < 65; i*=2) {
+    console.log(i);
+}
+*/
+
+//Part 3
+for (var i = 1; i < 4; i++) {
+    for (var j = 1; j < 4; j++) {
+        console.log(i);
+    }
+}
+
+
